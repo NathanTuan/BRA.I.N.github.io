f17011e28bbcbf824550eed15dbd40e2c495fb46
Nathan Tuan <nathancyt2007@gmail.com>
2026-05-17 20:56:54 -0700

    updating repo

---
+++ app.js
@@ -0,0 +1,19 @@
+
+/*
+async function analyzeImage(file) {
+    const formData = new FormData();
+    formData.append("file", file);
+
+    const response = await fetch(
+        "https://brain-website-backend.onrender.com/predict",
+        {
+            method: "POST",
+            body: formData
+        }
+    );
+
+    const data = await response.json();
+    console.log(data);
+    return data;
+}
+*/
